da93e622db2300b1333b5518d93c4336b9b1efe9
Gadjib <ilya@andrianov.ru>
2019-10-22 16:50:24 +0300

    Ы

--- 9 класс/CW 23-09-2019/p6.ml
+++ 9 класс/CW 23-09-2019/p6.ml
@@ -2,18 +2,18 @@ open Unix;;
 
 let f = open_out_bin "file.txt";;
 
-let to_utf_8 n = if n < 128 then 0 lsl n else
-				(if n < 256 then 110 lsl n else
-				(if n < 384 then 1110 lsl n else 11110 lsl n));;
+let to_utf_8 n f = if (int_of_float ((2.**32.)-1)) lxor (int_of_float ((2.**7.)-1)) = 0 then output_byte f (n land 0xFF) else
+				(if (int_of_float ((2.**32.)-1)) lxor (int_of_float ((2.**15.)-1)) = 0 then output_byte f ((n land (0xFF lsl 8)) lsr 8);
+				                                                                            output_byte f (n land 0xFF) else
+				(if (int_of_float ((2.**32.)-1)) lxor (int_of_float ((2.**23.)-1)) = 0 then output_byte f ((n land (0xFF lsl 16)) lsr 16);
+					                                                                        output_byte f ((n land (0xFF lsl 8)) lsr 8);
+				                                                                            output_byte f (n land 0xFF)
+				else (output_byte f ((n land (0xFF lsl 24)) lsr 24);
+				     output_byte f ((n land (0xFF lsl 16)) lsr 16);
+					 output_byte f ((n land (0xFF lsl 8)) lsr 8);
+				     output_byte f (n land 0xFF))));;
 
-let write_alphabeth() =
-	for i = 176 to 191 do
-		output_byte f (to_utf_8 i);
-	done;
-	for i = 128 to 143 do
-		output_byte f (to_utf_8 i);
-	done;
-;;
+to_utf_8 48 f;;
 
 write_alphabeth();;
 close_out f;;
