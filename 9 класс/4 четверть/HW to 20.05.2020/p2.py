d979c125d671f8c56b2898b2ad489701f4bd0220
Gadjib <ilya@andrianov.ru>
2020-05-19 22:42:33 +0300

    А теперь вторую

---
+++ 9 класс/4 четверть/HW to 20.05.2020/p2.py
@@ -0,0 +1,21 @@
+import re
+
+with open("p2.txt") as f:
+    content = f.readlines()
+
+EMAIL_REGEX = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
+D = {}
+for x in content:
+    tokens = x.strip().split(' ')
+    if re.match(EMAIL_REGEX, tokens[0]):
+        email = tokens[0]
+        name = tokens[1]
+        surname = tokens[2]
+    else:
+        name = tokens[0]
+        surname = tokens[1]
+        email = None
+    if surname not in D or not D[surname]:
+        D[surname] = name
+
+print(D)
